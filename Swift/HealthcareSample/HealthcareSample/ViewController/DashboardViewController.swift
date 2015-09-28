3e68cad215d0959dfcab64713c635e860e98a615
越智 宗洋 <ochi_m@mti.co.jp>
2015-09-28 16:37:09 +0900

    ダッシュボードのレイアウト作成。ヘルスケアから値の読み込み

--- Swift/HealthcareSample/HealthcareSample/ViewController/DashboardViewController.swift
+++ Swift/HealthcareSample/HealthcareSample/ViewController/DashboardViewController.swift
@@ -9,17 +9,57 @@
 import UIKit
 
 class DashboardViewController: UIViewController {
+    var heights: [HealthItem]? = []
+    var weights: [HealthItem]? = []
+    var pulses: [HealthItem]? = []
+    
+    @IBOutlet weak var mySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var myTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         var height: Height = Height()
-        //height.findAllItemValues({responseObj, error in})
+        height.findAllItemsOnHealthcare({healthItems, error in
+            
+            if self.showErrorAlert(error) {
+                return
+            }
+            self.heights! = healthItems!
+        })
+        
+        var weight: Weight = Weight()
+        weight.findAllItemsOnHealthcare({healthItems, error in
+            
+            if self.showErrorAlert(error) {
+                return
+            }
+            self.weights! = healthItems!
+        })
+        
+        var pulse: Pulse = Pulse()
+        pulse.findAllItemsOnHealthcare({healthItems, error in
+            
+            if self.showErrorAlert(error) {
+                return
+            }
+            self.pulses! = healthItems!
+        })
+
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func showErrorAlert(error :NSError?) -> Bool {
+        if error != nil {
+            var builder:SimpleAlertBuilder = SimpleAlertBuilder(title: "", message: error!.localizedDescription)
+            self.presentViewController(builder.alertController, animated: true, completion: nil)
+            return true
+        }
+        return false
     }
     
 }
